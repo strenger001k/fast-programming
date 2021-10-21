48f4aa60a771c0f40d2a7098adfc41a1e4ecadc8
stranger001k <zhenia0072@gmail.com>
2021-10-21 17:34:37 +0300

    pep8

--- timer.py
+++ timer.py
@@ -1,13 +1,14 @@
 import time
 
-import main1, main2, main3, create_txt
+import main1, main2, main3
+import create_txt
+
 
 def timer(method):
-    
     def get():
         start = time.time()
         function = method()
-        print ("time: %f" % (time.time()-start))
+        print("time: %f" % (time.time()-start))
         return function
 
     return get
@@ -15,25 +16,28 @@ def timer(method):
 
 @timer
 def function1():
-	main1.start()
+    main1.start()
+
 
 @timer
 def function2():
     main2.start()
-    
+
+
 @timer
 def function3():
     main3.start()
 
+
 if __name__ == '__main__':
     print("RESET")
     create_txt.reload()
-    function1() #асинхронно
+    function1()  # асинхронно
 
     print("RESET")
     create_txt.reload()
-    function2() #многопроцессорно 
+    function2()  # многопроцессорно
 
     print("RESET")
     create_txt.reload()
-    function3() #многопоточно
+    function3()  # многопоточно
