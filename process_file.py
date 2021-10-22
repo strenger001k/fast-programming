ef4571788c14d36a1db734cdd7c3a569b9f8aa19
stranger001k <zhenia0072@gmail.com>
2021-10-22 14:01:17 +0300

    upgrade

--- process_file.py
+++ process_file.py
@@ -1,6 +1,5 @@
 def replace_symbol(content):
     text = list(content)
-    for i in range(len(text)):
-        if (i+1) % 5 == 0:
+    for i in range(4, len(text), 5):
             text[i] = "*"
     return text
